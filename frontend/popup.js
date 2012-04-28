bbdc6118ca2d4ee5926b31015547afc7323400fc
doktor500 <davidmolinero.com@gmail.com>
2012-04-28 13:21:39 +0200

    Integration between frontend and backend

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,37 +1,21 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var bookTitle = 'X unit patterns';
 
-var req = new XMLHttpRequest();
-req.open(
-    "GET",
-    "http://api.flickr.com/services/rest/?" +
-        "method=flickr.photos.search&" +
-        "api_key=90485e931f687a9b9c2a66bf58a3861a&" +
-        "text=hello%20world&" +
-        "safe_search=1&" +  // 1 is "safe"
-        "content_type=1&" +  // 1 is "photos only"
-        "sort=relevance&" +  // another good one is "interestingness-desc"
-        "per_page=20",
-    true);
-req.onload = showPhotos;
-req.send(null);
-
-function showPhotos() {
-  var photos = req.responseXML.getElementsByTagName("photo");
-
-  for (var i = 0, photo; photo = photos[i]; i++) {
-    var img = document.createElement("image");
-    img.src = constructImageURL(photo);
-    document.body.appendChild(img);
-  }
+function showBookLinks() {    
+    $.getJSON('http://192.168.3.65:8080/torrent-server/torrentHub/index?bookTitle=' + bookTitle, function(data) {
+      $.each(data.torrents, function(key, val) {
+        appendBook(val.url, val.title);
+      });
+    });
 }
 
-// See: http://www.flickr.com/services/api/misc.urls.html
-function constructImageURL(photo) {
-  return "http://farm" + photo.getAttribute("farm") +
-      ".static.flickr.com/" + photo.getAttribute("server") +
-      "/" + photo.getAttribute("id") +
-      "_" + photo.getAttribute("secret") +
-      "_s.jpg";
+function appendBook(url, title) {
+  var item = document.createElement('li');
+  var bookLink = document.createElement('a');
+  bookLink.setAttribute('href', url);
+  bookLink.setAttribute('target', "_blank");
+  bookLink.appendChild(document.createTextNode(title));  
+  item.appendChild(bookLink);
+  document.getElementById('torrents').appendChild(item);  
 }
+
+showBookLinks();
