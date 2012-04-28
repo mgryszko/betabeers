66d0e651427e4cc8c05b89f9e3bbd49f83640f67
Arturo Herrero <arturo.herrero@gmail.com>
2012-04-28 11:51:16 +0200

    Build a JSON example with JsonBuilder

--- backend/torrent-server/grails-app/controllers/betabeers/TorrentHubController.groovy
+++ backend/torrent-server/grails-app/controllers/betabeers/TorrentHubController.groovy
@@ -1,6 +1,20 @@
 package betabeers
 
+import groovy.json.*
+
+
 class TorrentHubController {
 
-    def index() { }
+    def index() {
+        def json = new JsonBuilder()
+        json.torrents {
+            torrent {
+                title 'Neuro-Linguistic Programming'
+                link 'http://www.mnova.eu/rss/download/torrent/211231/2008-07-13/0a403d196cb8cef927fdc5231e3a5b014d562b98/e61c8b69242a8634f63fbc9717d47519/Neuro-Linguistic_Programming_-_NLP_eBook_(www.softzone.org).torrent'
+            }
+        }
+        render json.toString()
+    }
+
 }
+ 
